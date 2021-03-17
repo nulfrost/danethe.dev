bbfa50494709562e95c7832f298df4749f1a3dc7
Danex2 <khadane.miller@gmail.com>
2021-03-17 02:00:43 -0400

    remove remaining contentful code

--- lib/contentful.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "contentful";
-
-const contentful = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-});
-
-export { contentful };
