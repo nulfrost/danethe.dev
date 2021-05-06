44d65aee03de9e97da59f5bf49afd5cc8b84fe1f
Danex2 <khadane.miller@gmail.com>
2021-05-06 18:21:49 -0400

    add some padding on layout

--- components/sections/Layout.tsx
+++ components/sections/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
-      <div className="max-w-[1280px] mx-auto font-sans px-4 md:px-0 pb-20">
+      <div className="max-w-[1280px] mx-auto font-sans px-7 xl:px-0 pb-20">
         <Header />
         <main>{children}</main>
       </div>
