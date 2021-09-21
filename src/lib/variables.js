ef5572afe1cd0f1403bdcf35086f3c89061d1d25
Dane Miller <khadane.miller@gmail.com>
2021-09-21 01:38:55 -0400

    add notion stuff back, refactor a bit of code, svelte is cool

---
+++ src/lib/variables.js
@@ -0,0 +1,4 @@
+export const variables = {
+	notionToken: import.meta.env.VITE_NOTION_TOKEN,
+	notionDatabase: import.meta.env.VITE_NOTION_DATABASE
+};
--- src/routes/reading.svelte
+++ src/routes/reading.svelte
@@ -1,30 +1,31 @@
+<script context="module">
+	export async function load() {
+		const res = await fetch('http://localhost:3000/api/books');
+		const reading = await res.json();
+
+		return {
+			props: {
+				reading: reading?.books
+			}
+		};
+	}
+</script>
+
 <script>
-	const books = [
-		{
-			title: 'Refactoring: Improving the Design of Existing Code',
-			author: 'Martin Fowler',
-			link: '#',
-			status: 'reading'
-		},
-		{
-			title: 'Refactoring: Improving the Design of Existing Code',
-			author: 'Martin Fowler',
-			link: '#',
-			status: 'reading'
-		},
-		{
-			title: 'Refactoring: Improving the Design of Existing Code',
-			author: 'Martin Fowler',
-			link: '#',
-			status: 'reading'
-		},
-		{
-			title: 'Refactoring: Improving the Design of Existing Code',
-			author: 'Martin Fowler',
-			link: '#',
-			status: 'reading'
+	function displayReadingStatus(status) {
+		switch (status) {
+			case 'Finished':
+				return 'text-green-800 bg-green-50';
+			case 'Reading':
+				return 'text-blue-800 bg-blue-50';
+			case 'Paused':
+				return 'text-yellow-800 bg-yellow-50';
+			case 'Ready to Start':
+				return 'text-red-800 bg-red-50';
 		}
-	];
+	}
+
+	export let reading;
 </script>
 
 <svelte:head>
@@ -34,19 +35,26 @@
 <section class="mt-[112px]">
 	<h1 class="text-5xl font-bold mb-4">Reading</h1>
 	<div class="grid grid-cols-2 gap-6">
-		{#each books as book}
+		{#each reading as book}
 			<div
 				tabindex="0"
 				class="border-2 rounded px-5 py-3 h-40 hover:border-purple-600 flex flex-col focus:outline-none focus:border-purple-600"
 			>
-				<h2 class="text-lg font-semibold mb-2">{book.title}</h2>
-				<p class="text-gray-500">{book.author}</p>
+				<h2 class="text-lg font-semibold mb-2">{book.properties.Name.title[0].plain_text}</h2>
+				<p class="text-gray-500">
+					{book.properties.Author.multi_select.map((author) => author.name).join(', ')}
+				</p>
 				<div class="mt-auto flex">
-					<small class="text-yellow-800 font-semibold bg-yellow-300 px-2 py-1 rounded"
-						>{book.status}</small
+					<small
+						class="font-semibold px-2 py-1 rounded {displayReadingStatus(
+							book.properties.Status.select.name
+						)}">{book.properties.Status.select.name}</small
 					>
-					<a href={book.link} class="ml-auto text-blue-500" rel="external" target="_blank"
-						>Link &rarr;</a
+					<a
+						href={book.properties.Link.url}
+						class="ml-auto text-blue-500"
+						rel="external"
+						target="_blank">Link &rarr;</a
 					>
 				</div>
 			</div>
