97feea3ea59d5d274e5b3e9b9d1d403252b638c9
Dane Miller <khadane.miller@gmail.com>
2021-09-25 10:47:00 -0400

    add filter for reading list

---
+++ src/components/Book.svelte
@@ -0,0 +1,34 @@
+<script>
+	function displayReadingStatus(status) {
+		switch (status) {
+			case 'Finished':
+				return 'text-green-800 bg-green-50';
+			case 'Reading':
+				return 'text-blue-800 bg-blue-50';
+			case 'Paused':
+				return 'text-yellow-800 bg-yellow-50';
+			case 'Ready to Start':
+				return 'text-red-800 bg-red-50';
+		}
+	}
+	export let properties;
+</script>
+
+<a
+	href={properties.Link.url}
+	title="External link to buy or access the book {properties.Name.title[0].plain_text}"
+	rel="external"
+	target="_blank"
+	class="border-2 rounded px-5 py-3 h-40 hover:border-purple-600 flex flex-col focus:outline-none focus:border-purple-600"
+>
+	<h2 class="text-lg font-semibold mb-2">{properties.Name.title[0].plain_text}</h2>
+	<p class="text-gray-500">
+		{properties.Author.multi_select.map((author) => author.name).join(', ')}
+	</p>
+	<div class="mt-auto flex">
+		<small
+			class="font-semibold px-2 py-1 rounded {displayReadingStatus(properties.Status.select.name)}"
+			>{properties.Status.select.name}</small
+		>
+	</div>
+</a>
--- src/routes/reading.svelte
+++ src/routes/reading.svelte
@@ -18,47 +18,49 @@
 </script>
 
 <script>
-	function displayReadingStatus(status) {
-		switch (status) {
-			case 'Finished':
-				return 'text-green-800 bg-green-50';
-			case 'Reading':
-				return 'text-blue-800 bg-blue-50';
-			case 'Paused':
-				return 'text-yellow-800 bg-yellow-50';
-			case 'Ready to Start':
-				return 'text-red-800 bg-red-50';
-		}
+	import Book from '../components/Book.svelte';
+	export let reading;
+	let filteredBooks = [];
+	let filter = '';
+
+	function setFilter(event) {
+		filter = event.target.outerText;
 	}
 
-	export let reading;
+	$: {
+		if (filter) {
+			filteredBooks = reading.filter(
+				(read) => read.properties.Status.select.name === event.target.outerText
+			);
+		} else {
+			filteredBooks = reading;
+		}
+	}
 </script>
 
 <svelte:head>
 	<title>Dane Miller - Reading</title>
 </svelte:head>
 
-<h1 class="text-4xl font-bold mb-4">Reading</h1>
-<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-	{#each reading as book}
-		<a
-			href={book.properties.Link.url}
-			title="External link to buy or access the book {book.properties.Name.title[0].plain_text}"
-			rel="external"
-			target="_blank"
-			class="border-2 rounded px-5 py-3 h-40 hover:border-purple-600 flex flex-col focus:outline-none focus:border-purple-600"
+<div class="flex justify-between items-baseline flex-col md:flex-row">
+	<h1 class="text-4xl font-bold mb-4">Reading</h1>
+	<div class="font-semibold mb-4 md:mb-0">
+		<span
+			class="text-red-800 bg-red-50 px-2 py-1 rounded hover:bg-red-100 duration-150 cursor-pointer"
+			on:click={setFilter}>Ready to Start</span
 		>
-			<h2 class="text-lg font-semibold mb-2">{book.properties.Name.title[0].plain_text}</h2>
-			<p class="text-gray-500">
-				{book.properties.Author.multi_select.map((author) => author.name).join(', ')}
-			</p>
-			<div class="mt-auto flex">
-				<small
-					class="font-semibold px-2 py-1 rounded {displayReadingStatus(
-						book.properties.Status.select.name
-					)}">{book.properties.Status.select.name}</small
-				>
-			</div>
-		</a>
+		<span
+			class="text-yellow-800 bg-yellow-50 px-2 py-1 rounded hover:bg-yellow-100 duration-150 cursor-pointer"
+			on:click={setFilter}>Paused</span
+		>
+		<span
+			class="text-green-800 bg-green-50 px-2 py-1 rounded hover:bg-green-100 duration-150 cursor-pointer"
+			on:click={setFilter}>Finished</span
+		>
+	</div>
+</div>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+	{#each filteredBooks as book}
+		<Book {...book} />
 	{/each}
 </div>
