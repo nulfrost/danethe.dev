6bb17bdd2c7d385b3b6cd551d81f3c9554343210
Dane Miller <khadane.miller@gmail.com>
2021-10-01 00:23:08 -0400

    start working on basic OG tags for website

---
+++ src/components/SEO.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let url;
+	export let type;
+	export let title;
+	export let description;
+</script>
+
+<svelte:head>
+	<meta property="og:title" content={title} />
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content={type} />
+	<meta property="og:description" content={description} />
+</svelte:head>
