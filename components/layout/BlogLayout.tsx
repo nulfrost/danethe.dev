d588c476a6e6bc76165903a5f2c6bf4d86b2c8fe
Dane Miller <khadane.miller@gmail.com>
2022-01-05 21:15:19 -0500

    working on seo for blog posts

--- components/layout/BlogLayout.tsx
+++ components/layout/BlogLayout.tsx
@@ -16,6 +16,16 @@ export function BlogLayout({ meta, children }) {
           description: meta.description,
           url: typeof window !== "undefined" && window.location.href,
           type: "article",
+          article: {
+            publishedTime: new Date(meta.data).toISOString(),
+            tags: meta.tags
+          },
+          images: [
+            {
+              url: `https://og-image-six-ashen.vercel.app/**${meta.title}**.png?md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets`,
+              alt: meta.title
+            }
+          ]
         }}
       />
       <div className="mt-24 prose max-w-none">{children}</div>
