0023606b9f6b28213b6a7aa9cff1f33b2d6552a5
Dane Miller <khadane.miller@gmail.com>
2022-02-28 16:26:20 -0500

    add colour design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+     colors: {
+       primary: "#242323",
+       text: "#DADAD2",
+       secondary: "#6E7480",
+       "secondary-2": "#535C36",
+      "secondary-3": "#766A74",
+      "secondary-4": "#C59D7A",
+       highlight: "#C47975",
+     }
+    },
   },
   plugins: [],
 }
