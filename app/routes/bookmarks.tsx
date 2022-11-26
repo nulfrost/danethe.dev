a927a0a3868aca3a86a46a9ea76b49c8d8a1a95c
Dane Miller <khadane.miller@gmail.com>
2022-11-25 19:18:24 -0500

    make grid columns same height

--- app/routes/bookmarks.tsx
+++ app/routes/bookmarks.tsx
@@ -25,7 +25,7 @@ export default function Bookmarks() {
       <h1 className="mt-4 text-2xl font-bold text-snes-black mb-4">
         Bookmarks
       </h1>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 auto-cols-fr">
         {bookmarks.results.map((bookmark) => (
           <li key={bookmark.id}>
             <a
@@ -33,12 +33,12 @@ export default function Bookmarks() {
               target="_blank"
               rel="noreferrer noopener"
               aria-label={`${bookmark?.properties?.Title?.title[0].plain_text}, opens in a new tab`}
-              className="[border-style:outset] border-2 border-white p-3 inline-block hover:[border-style:inset] focus-visible:outline-4 focus-visible:outline-offset-1 focus-visible:outline-snes-purple focus-visible:outline-dashed border-none focus-visible:underline hover:underline w-full"
+              className="[border-style:outset] border-2 border-white p-3 inline-block hover:[border-style:inset] focus-visible:outline-4 focus-visible:outline-offset-1 focus-visible:outline-snes-purple focus-visible:outline-dashed border-none focus-visible:underline hover:underline w-full h-full flex flex-col"
             >
               <h2 className="font-bold">
                 {bookmark?.properties?.Title?.title[0].plain_text}
               </h2>
-              <p className="text-sm">
+              <p className="text-sm mt-auto">
                 {bookmark?.properties?.Author.rich_text[0]?.plain_text}
               </p>
             </a>
