1c077e8e4f464163c63fda2ac9a63ab319f3894f
Dane Miller <khadane.miller@gmail.com>
2023-08-10 22:50:13 -0400

    trying out astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "snes-black": "var(--snes-black)",
+        "snes-light-gray": "var(--snes-light-gray)",
+        "snes-dark-gray": "var(--snes-dark-gray)",
+        "snes-purple": "var(--snes-purple)",
+        "snes-light-purple": "var(--snes-light-purple)",
+      },
+      fontFamily: {
+        sans: ["AsapVariable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
