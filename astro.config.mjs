d4844bb3ed8cb3de5f4de297eb4d1ed8d3407b69
Dane Miller <khadane.miller@gmail.com>
2023-08-12 22:55:40 -0400

    meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://danethe.dev",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
